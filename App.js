495945ea31e642b8e3112cd7d355c4f073077fd4
Rohit Vyas <77916479+Draconus524@users.noreply.github.com>
2021-05-16 01:46:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import LoginPage from './screens/LoginPage';
 import Stores from './screens/Stores';
 import StoreView from './screens/StoreView';
+import About from './screens/About';
+import PaymentSucessfull from './screens/PaymentSucessfull';
 
 export default function App() {
   return (
     <LoginPage />
     // <Stores />
     // <StoreView />
+    //<About />
+    //<PaymentSucessfull />
   );
 }
 
