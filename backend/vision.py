1716a69695dd74b51008fa1eee3bbb99226d6bd1
root <root@DESKTOP-1N126KV.localdomain>
2021-05-16 04:10:47 -0400

    backend directory now same as in backend repo

--- backend/vision.py
+++ backend/vision.py
@@ -5,7 +5,7 @@ from google.oauth2 import service_account
 from dotenv import load_dotenv
 
 load_dotenv()
-KEYDIR_PATH = os.getenv('KEYDIR_PATH')
+KEYDIR_PATH = os.getenv(key='KEYDIR_PATH')
 credentials = service_account.Credentials.from_service_account_file(KEYDIR_PATH)
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
@@ -15,24 +15,21 @@ def detect_objects(image_path: str):
 
     Args:
         image_path (str): Path to image file.
-    
+
     Returns:
         objects: Annotated image object of image given by path.
     """
-    image_file = open(file=image_path, mode='rb')
+    image_file = open(file=image_path, mode="rb")
     content = image_file.read()
     image = vision.Image(content=content)
     objects = client.object_localization(image=image).localized_object_annotations
 
-    print(type(objects))
-    print(f'Number of objects found: {len(objects)}')
-    for object_ in objects:
-        print(f'\n{object_.name} (confidence: {object_.score})')
-        print('Normalized bounding polygon vertices: ')
-        for vertex in object_.bounding_poly.normalized_vertices:
-            print(f' - ({vertex.x}, {vertex.y})')
+    # to prevent a really weird exception
+    if len(objects) == 0:
+        return []
+    else:
+        return [i for i in objects]
 
-    return objects
 
 def detect_logos(image_path: str):
     """
@@ -42,33 +39,41 @@ def detect_logos(image_path: str):
         image_path (str): Path to image file.
 
     Returns:
-        object: 
+        object:
     """
 
-    image_file = open(file=image_path, mode='rb')
+    image_file = open(file=image_path, mode="rb")
     content = image_file.read()
     image = vision.Image(content=content)
     response = client.logo_detection(image=image)
     logos = response.logo_annotations
-    
-    print(type(response))
-    print('Logos:')
-    for logo in logos:
-        print(logo.description)
 
     if response.error.message:
         raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
 
-    return logos
+    # to prevent a really weird exception
+    if len(logos) == 0:
+        return []
+    else:
+        return [i for i in logos]
 
-if __name__ == '__main__':
-    print('-' * 100)
-    image_path = './data/fruits.png'
+
+if __name__ == "__main__":
+    print("-" * 100)
+    image_path = "./data/fruits.png"
     objects = detect_objects(image_path=image_path)
 
-    print('-' * 100)
-    image_path = './data/logo-collage.png'
+    for object_ in objects:
+        print(f"\n{object_.name} (confidence: {object_.score})")
+        print("Normalized bounding polygon vertices: ")
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(f" - ({vertex.x}, {vertex.y})")
+
+    print("-" * 100)
+    image_path = "./data/quarantine-processed-foods.png"
     logos = detect_logos(image_path=image_path)
+    for logo in logos:
+        print(logo.description)
